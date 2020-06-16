b665dcf73124ae148472f70176c63351e5106e74
Dave <dave.cloud.devops@gmail.com>
2020-06-15 21:16:13 -0500

    Tuplas Python

---
+++ 8 - Tuplas_I.py
@@ -0,0 +1,13 @@
+mitupla=("Juan", "Pedreo", 13, 1 , 1995) # se abren parentesis
+milista=list(mitupla) #imprime una lista
+
+print(mitupla[2]) #impresion normal de tuplas
+
+print(milista) #imprime una lista
+
+milista=["Juan", "pedro"]
+mitupla=tuple(milista)
+print(mitupla)
+
+
+
