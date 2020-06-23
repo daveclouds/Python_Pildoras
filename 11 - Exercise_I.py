3d4cfed392f18a20df1df500f7624f90cd523624
Dave <dave.cloud.devops@gmail.com>
2020-06-22 20:06:29 -0500

    Conditional IF and 2 Excercises

---
+++ 11 - Exercise_I.py
@@ -0,0 +1,11 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+num3 = int(input("Enter third number: "))
+
+def average(num1, num2, num3):
+    return (num1 + num2 + num3) / 3
+     
+print("The average is: ")
+average(num1, num2, num3)
+
+
---
+++ 11 - Lista_Exercise.py
@@ -0,0 +1,14 @@
+name = input("Enter your name: ")
+addrees = input("Enter your address: ")
+phone = input("Enter your phone: ")
+
+datauser = [name, addrees, phone]
+
+print("Your information is: " + datauser[0] + " " + datauser[1] + " " + datauser[2])
+
+
+
+#Crea un programa que pida por teclado “Nombre”, “Dirección” y “Tfno”. Esos tres datos
+#deberán ser almacenados en una lista y mostrar en consola el mensaje: “Los datos
+#personales son: nombre apellido teléfono” (Se mostrarán los datos introducidos por
+#teclado).
